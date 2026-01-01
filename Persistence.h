15bfe924832d9adfef44c0b40476d15ca76e8587
almastelek <almasatelek@gmail.com>
2026-01-01 14:07:24 -0500

    updated persistence api

--- Persistence.h
+++ Persistence.h
@@ -4,12 +4,13 @@
 #include <fstream>
 #include <sstream>
 #include "GradeManager.h"
+#include "CourseManager.h"
 
 class Persistence {
 public:
-    static void saveToFile(const std::string& filename, const GradeManager& manager);
+    static void saveToFile(const std::string& filename, const CourseManager& manager);
 
-    static void loadFromFile(const std::string& filename, GradeManager& manager);
+    static void loadFromFile(const std::string& filename, CourseManager& manager);
 };
 
 #endif // PERSISTENCE_H
